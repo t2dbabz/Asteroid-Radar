3088a9a596ec9521248f1a83732a88c29b83a944
Babatunde Owoleke <tundeowoleke1@gmail.com>
2021-12-25 11:59:37 +0100

    Query to retrieve asteroids of the day and week

--- app/src/main/java/com/udacity/asteroidradar/database/AsteroidDao.kt
+++ app/src/main/java/com/udacity/asteroidradar/database/AsteroidDao.kt
@@ -9,11 +9,17 @@ import androidx.room.Query
 @Dao
 interface AsteroidDao {
 
-    @Query("SELECT * FROM asteroids_table")
+    @Query("SELECT * FROM asteroids_table ORDER BY closeApproachDate ASC")
     fun getAsteroid(): LiveData<List<DatabaseAsteroid>>
 
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertAll(vararg asteroid: DatabaseAsteroid)
+
+    @Query("SELECT * FROM asteroids_table WHERE closeApproachDate  BETWEEN :startDate AND :endDate ORDER BY closeApproachDate ASC")
+    fun getWeekAsteroids(startDate: String, endDate: String): LiveData<List<DatabaseAsteroid>>
+
+    @Query("SELECT * FROM asteroids_table WHERE closeApproachDate == :todayDate ORDER BY closeApproachDate ASC ")
+    fun getTodayAsteroids(todayDate: String): LiveData<List<DatabaseAsteroid>>
 
 }
